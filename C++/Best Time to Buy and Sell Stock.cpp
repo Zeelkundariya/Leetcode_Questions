34f81df57460fc946bf3528ccd603fdb7572f360
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-03-30 23:25:50 +0530

    leetcode Questiom 3 done 30-03-26

---
+++ C++/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,24 @@
+//     #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int minPrice = prices[0];
+//         int maxProfit = 0;
+
+//         for(int i = 1; i < prices.size(); i++){
+//             if (prices[i] < minPrice){
+//                 minPrice = prices[i];
+//             }
+//             else{
+//                 int profit = prices[i] - minPrice;
+//                 if(profit > maxProfit){
+//                     maxProfit = profit;
+//                 }
+//             }
+//         }
+//         return maxProfit;
+//     }
+// };

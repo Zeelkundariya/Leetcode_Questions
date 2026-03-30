ebc3c590af1836929466944913858201fb5b3a69
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-03-30 21:17:26 +0530

    Find greatest common divisor of array

---
+++ Javascript/Find Greatest Common Divisor of Array.js
@@ -0,0 +1,14 @@
+var findGCD = function(nums) {
+    let min = Math.min(...nums);
+    let max = Math.max(...nums);
+
+    function gcd(a,b){
+        while(b !==0){
+            let temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+    return gcd(min,max)
+}

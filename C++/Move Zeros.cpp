129cdcf188f0f034a0ec35fa1f3ea4838c3879f2
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-04-06 22:58:55 +0530

    Total Questions- 16 6-04-2026

---
+++ C++/Move Zeros.cpp
@@ -0,0 +1,12 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int j=0;
+//         for(int i =1; i<nums.size(); i++){
+//             if(nums[i] != 0){
+//                 swap(nums[i], nums[j]);
+//                 j++;
+//             }
+//         }
+//     }
+// };

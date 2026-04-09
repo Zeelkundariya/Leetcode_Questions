c2256718c60195b277a3b5bfc6ae99649215df0e
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-04-09 05:55:31 +0530

    Total Questions-19  6-04-2026

--- C++/412. Fuzz Buzz.cpp
+++ C++/412. Fuzz Buzz.cpp
@@ -1,22 +1,22 @@
 // class Solution {
 // public:
 //     vector<string> fizzBuzz(int n) {
-//         vector<string> answer;
+//         vector<string> ans;
 
-//         for(int i=1;i<=n;i++){
+//         for(int i =1; i<=n;i++){
 //             if(i%3==0 && i%5==0){
-//                 answer.push_back("FizzBuzz");
+//                 ans.push_back("FizzBuzz");
 //             }
-//             else if(i%3==0){
-//                 answer.push_back("Fizz");
+//             else if(i%3 == 0){
+//                 ans.push_back("Fizz");
 //             }
 //             else if(i%5==0){
-//                 answer.push_back("Buzz");
+//                 ans.push_back("Buzz");
 //             }
 //             else{
-//                 answer.push_back(to_string(i));
-//             }
+//                   ans.push_back(to_string(i));
+//                 }
 //         }
-//         return answer;
+//         return ans;
 //     }
 // };

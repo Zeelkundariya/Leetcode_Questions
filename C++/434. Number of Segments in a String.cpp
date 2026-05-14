de8155dc28938259b60934a28ad013b98978dc99
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-05-14 09:49:26 +0530

    Total Questions-34

---
+++ C++/434. Number of Segments in a String.cpp
@@ -0,0 +1,16 @@
+// class Solution {
+// public:
+//     int countSegments(string s) {
+
+//         int count = 0;
+//         for(int i = 0; i < s.size(); i++) {
+//             if(s[i] != ' ') {
+//                 count++;
+//                 while(i < s.size() && s[i] != ' ') {
+//                     i++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };

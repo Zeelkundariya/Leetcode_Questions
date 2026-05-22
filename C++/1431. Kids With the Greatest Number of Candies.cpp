433406a7a9b5ffa04e2afa81f6675f2cf42244d9
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-05-22 14:52:13 +0530

    Total Questions 64

---
+++ C++/1431. Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,19 @@
+// class Solution {
+// public:
+//     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+//         vector<bool> ans;
+
+//         for(int i = 0; i < candies.size(); i++) {
+//             bool greatest = true;
+//             for(int j = 0; j < candies.size(); j++) {
+//                 if(candies[i] + extraCandies < candies[j]) {
+//                     greatest = false;
+//                     break;
+//                 }
+//             }
+//             ans.push_back(greatest);
+//         }
+//         return ans;
+//     }
+// };

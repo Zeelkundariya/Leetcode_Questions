b91dec0dc8141b46fbcae912eeeabc28cf545dd4
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-05-27 09:20:11 +0530

    Total Questions- 81

---
+++ C++/1957. Delete Characters to Make Fancy String.cpp
@@ -0,0 +1,19 @@
+// class Solution {
+// public:
+//     string makeFancyString(string s) {
+//         std::string result = "";
+//         int count = 0;
+        
+//         for (int i = 0; i < s.length(); ++i) {
+//             if (i == 0 || s[i] == s[i - 1]) {
+//                 count++;
+//             } else {
+//                 count = 1;
+//             }
+//             if (count < 3) {
+//                 result += s[i];
+//             }
+//         }
+//         return result;
+//     }
+// };

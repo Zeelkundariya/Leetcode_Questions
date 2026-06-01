8891cfdf8592f9938d4e27a6a231f13353e9acb2
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-01 11:04:42 +0530

    Total QUestions 88

---
+++ C++/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
@@ -0,0 +1,33 @@
+// class Solution {
+// public:
+//     int isPrefixOfWord(string sentence, string searchWord) {
+//         string word = "";
+//         int position = 1;
+//         for(int i=0; i<= sentence.size();i++){
+//             if( i == sentence.size() || sentence[i] == ' '){
+//                 bool match = true;
+
+//                 if(searchWord.size() > word.size()){
+//                     match = false;
+//                 }
+//                 else{
+//                     for(int j = 0 ; j< searchWord.size(); j++){
+//                         if(word[j] != searchWord[j]){
+//                             match =false;
+//                             break;
+//                         }
+//                     }
+//                 }
+//                 if(match){
+//                     return position;
+//                 }
+//                 position++;
+//                 word= "";
+//             }
+//             else{
+//                 word += sentence[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };

a917bdf0810d0a061e6f48ecd3b7ee1b734adc0a
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-05 13:25:22 +0530

    Total Questions 92

--- C++/1047. Remove All Adjacent Duplicates In String.cpp
+++ C++/1047. Remove All Adjacent Duplicates In String.cpp
@@ -2,7 +2,7 @@
 // public:
 //     string removeDuplicates(string s) {
 
-//         string ans = ""
+//         string ans = "";
 //         for(int i = 0; i < s.size(); i++) {
 //             if(!ans.empty() && ans.back() == s[i]) {
 //                 ans.pop_back();

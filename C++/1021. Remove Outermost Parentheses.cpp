97b1466ffe59753d8581ae7c6c4a6fc8711acdff
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-09 15:05:53 +0530

    Total Questions-97

---
+++ C++/1021. Remove Outermost Parentheses.cpp
@@ -0,0 +1,28 @@
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+
+//         string ans = "";
+//         int count = 0;
+//         int start = 0;
+
+//         for(int i = 0; i < s.size(); i++) {
+
+//             if(s[i] == '(') {
+//                 count++;
+//             }
+//             else {
+//                 count--;
+//             }
+
+//             if(count == 0) {
+
+//                 ans += s.substr(start + 1, i - start - 1);
+
+//                 start = i + 1;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };

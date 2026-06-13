20dd2d4b99c086b72dc319bccdeb6ee936b8f590
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-13 23:55:36 +0530

    fix code-367

--- C++/367. Valid Perfect Square.cpp
+++ C++/367. Valid Perfect Square.cpp
@@ -1,23 +1,23 @@
-class Solution {
-public:
-    bool isPerfectSquare(int num) {
-        long long low = 1, high = num;
+// class Solution {
+// public:
+//     bool isPerfectSquare(int num) {
+//         long long low = 1, high = num;
 
-        while(low <= high){
-            long long mid = low + (high - low) / 2;
-            long long square = mid*mid;
+//         while(low <= high){
+//             long long mid = low + (high - low) / 2;
+//             long long square = mid*mid;
 
-            if(square == num){
-                return true;
+//             if(square == num){
+//                 return true;
 
-            }
-            else if(square < num){
-                low = mid+1;
-            }
-            else {
-                high = mid-1;
-            }
-        }
-            return false;
-        }
-};
+//             }
+//             else if(square < num){
+//                 low = mid+1;
+//             }
+//             else {
+//                 high = mid-1;
+//             }
+//         }
+//             return false;
+//         }
+// };

b1dc4d8690ba20617f7a0724db4a37d41d74f536
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-13 15:38:54 +0530

    Total Questions-102

---
+++ C++/2610. Convert an Array Into a 2D Array With Conditions.cpp
@@ -0,0 +1,22 @@
+// class Solution {
+// public:
+//     vector<vector<int>> findMatrix(vector<int>& nums) {
+
+//         vector<vector<int>> ans;
+//         unordered_map<int,int> mp;
+
+//         for(int num : nums) {
+
+//             int row = mp[num];
+
+//             if(row == ans.size()) {
+//                 ans.push_back({});
+//             }
+
+//             ans[row].push_back(num);
+//             mp[num]++;
+//         }
+
+//         return ans;
+//     }
+// };

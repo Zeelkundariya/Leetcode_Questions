2a40a0635450a0e26fd965e648ec5a7f6df1743c
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-13 10:23:54 +0530

    Total Questions-100

---
+++ C++/3945. Digit Frequency Score.cpp
@@ -0,0 +1,22 @@
+// class Solution {
+// public:
+//     int score(int n) {
+
+//         int freq[10] = {0};
+
+//         while(n > 0) {
+
+//             int digit = n % 10;
+//             freq[digit]++;
+//             n /= 10;
+//         }
+//         int ans = 0;
+
+//         for(int i = 0; i <= 9; i++) {
+
+//             ans += i * freq[i];
+//         }
+
+//         return ans;
+//     }
+// };

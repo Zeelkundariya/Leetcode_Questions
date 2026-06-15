b86f21a72ca35537c08b518e6e18bd08dcc7b1b2
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-15 10:55:17 +0530

    Total Question 107 method-1 updated

--- C++/438. Find All Anagrams in a String.cpp
+++ C++/438. Find All Anagrams in a String.cpp
@@ -1,22 +1,24 @@
 //Method-1
-class Solution {
-public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> ans;
 
-        sort(p.begin(), p.end());
 
-        int k = p.size();
+// class Solution {
+// public:
+//     vector<int> findAnagrams(string s, string p) {
+//         vector<int> ans;
 
-        for (int i = 0; i <= s.size() - k; i++) {
-            string temp = s.substr(i, k);
+//         sort(p.begin(), p.end());
 
-            sort(temp.begin(), temp.end());
+//         int k = p.size();
+
+//         for (int i = 0; i <= s.size() - k; i++) {
+//             string temp = s.substr(i, k);
+
+//             sort(temp.begin(), temp.end());
             
-            if (temp == p) {
-                ans.push_back(i);
-            }
-        }
-        return ans;
-    }
-};
+//             if (temp == p) {
+//                 ans.push_back(i);
+//             }
+//         }
+//         return ans;
+//     }
+// };

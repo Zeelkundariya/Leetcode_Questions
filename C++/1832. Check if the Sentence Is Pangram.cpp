fb77fb1c3f8f761ed319c9e9c14d8f10f4e55c8c
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-15 10:01:58 +0530

    Total Questions-105 method-2

--- C++/1832. Check if the Sentence Is Pangram.cpp
+++ C++/1832. Check if the Sentence Is Pangram.cpp
@@ -17,3 +17,17 @@
 
 
 //method-2
+
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            if(sentence.find(ch) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

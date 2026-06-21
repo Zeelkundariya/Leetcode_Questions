4007de4f79bbb939265943060f6794d12d07c53e
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-21 15:18:38 +0530

    Total Questions - 114

--- C++/2119. A Number After a Double Reversal.cpp
+++ C++/2119. A Number After a Double Reversal.cpp
@@ -1,10 +1,1 @@
-// class Solution {
-// public:
-//     bool isSameAfterReversals(int num) {
-
-//         if(num == 0) {
-//             return true;
-//         }
-//         return num % 10 != 0;
-//     }
-// };
+//  

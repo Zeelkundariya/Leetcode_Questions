dd7fb56d9188e85717869d0e2b130f878ec07379
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-23 18:05:41 +0530

    Total Questions-115

--- C++/1009. Complement of Base 10 Integer.cpp
+++ C++/1009. Complement of Base 10 Integer.cpp
@@ -1,3 +1,4 @@
+
 // class Solution {
 // public:
 //     int bitwiseComplement(int n) {
@@ -9,18 +10,14 @@
 //         int place = 1;
 
 //         while(n > 0) {
-
 //             int bit = n % 2;
 
 //             if(bit == 0) {
 //                 ans += place;
 //             }
-
 //             place *= 2;
-
 //             n /= 2;
 //         }
-
 //         return ans;
 //     }
 // };

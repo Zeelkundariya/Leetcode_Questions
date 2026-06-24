9beb83ba8e737b2df8ee0c8d082ef1e4076aafa3
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-24 16:59:02 +0530

    Total Questions-116 method-2 added

--- C++/2283. Check if Number Has Equal Digit Count and Digit Value.cpp
+++ C++/2283. Check if Number Has Equal Digit Count and Digit Value.cpp
@@ -17,3 +17,28 @@
 //         return true;
 //     }
 // };
+
+
+
+//Method-2
+
+// class Solution {
+// public:
+//     bool digitCount(string num) {
+
+//         unordered_map<int, int> mp;
+
+//         for(char ch : num) {
+//             mp[ch - '0']++;
+//         }
+
+//         for(int i = 0; i < num.size(); i++) {
+
+//             if(mp[i] != num[i] - '0') {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// };

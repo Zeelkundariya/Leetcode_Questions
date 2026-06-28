533a6ecbe5e360eba287b3722abe4442b07460dc
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-06-28 11:28:56 +0530

    Total Questions-118 improved in code

---
+++ C++/7. Reverse Integer.cpp
@@ -0,0 +1,21 @@
+// Method-1
+
+// class Solution {
+// public:
+//     int reverse(int x) {
+
+//         int rev = 0;
+//         int int_max = INT_MAX;
+//         int int_min = INT_MIN;
+
+//         while(x != 0) {
+//             int digit = x % 10;
+//             if(rev > int_max / 10 && rev < int_min / 10) {
+//                 return 0;
+//             }
+//             rev = rev * 10 + digit;
+//             x /= 10;
+//         }
+//         return rev;
+//     }
+// };

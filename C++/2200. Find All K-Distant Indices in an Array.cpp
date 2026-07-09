1ff9651f4c99635832e3f260aa638353844bcfbb
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-09 20:04:47 +0530

    Total Questions 125 added time and space complexity

--- C++/2200. Find All K-Distant Indices in an Array.cpp
+++ C++/2200. Find All K-Distant Indices in an Array.cpp
@@ -1,17 +1,21 @@
-class Solution {
-public:
-    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+// class Solution {
+// public:
+//     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
 
-        vector<int> ans;
+//         vector<int> ans;
 
-        for(int i = 0; i < nums.size(); i++) {
-            for(int j = 0; j < nums.size(); j++) {
-                if(nums[j] == key && abs(i - j) <= k) {
-                    ans.push_back(i);
-                    break;
-                }
-            }
-        }
-        return ans;
-    }
-};
+//         for(int i = 0; i < nums.size(); i++) {
+//             for(int j = 0; j < nums.size(); j++) {
+//                 if(nums[j] == key && abs(i - j) <= k) {
+//                     ans.push_back(i);
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+//time O(n²)
+//space O(n)

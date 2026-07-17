3fb9600c7224330ccd54f9ec8d02ae47a3f41c76
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-17 17:51:12 +0530

    Total Questions 129

--- C++/438. Find All Anagrams in a String.cpp
+++ C++/438. Find All Anagrams in a String.cpp
@@ -1,24 +1,26 @@
-//Method-1
-
-
-// class Solution {
-// public:
-//     vector<int> findAnagrams(string s, string p) {
-//         vector<int> ans;
-
-//         sort(p.begin(), p.end());
-
-//         int k = p.size();
-
-//         for (int i = 0; i <= s.size() - k; i++) {
-//             string temp = s.substr(i, k);
-
-//             sort(temp.begin(), temp.end());
-            
-//             if (temp == p) {
-//                 ans.push_back(i);
-//             }
-//         }
-//         return ans;
-//     }
-// };
+class Solution { 
+public: 
+    vector<int> findAnagrams(string s, string p) { 
+        int k1 = p.size(); 
+        int k2 = s.size(); 
+        vector<int> ans; 
+        if(k2 < k1) return ans; 
+        vector<int> freqP(26, 0); 
+        vector<int> freqS(26, 0); 
+        for(int i = 0; i < k1; i++){ 
+            freqP[p[i]-'a']++; 
+            freqS[s[i]-'a']++; 
+        } 
+        if(freqP == freqS){ 
+            ans.push_back(0); 
+        } 
+        for(int i = k1; i < k2; i++){ 
+            freqS[s[i]-'a']++; 
+            freqS[s[i-k1]-'a']--; 
+            if(freqP == freqS){ 
+                ans.push_back(i - k1 + 1); 
+            } 
+        } 
+        return ans; 
+    } 
+};

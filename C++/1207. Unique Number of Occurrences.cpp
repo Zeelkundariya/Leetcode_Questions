8b11b018dbe85aab48afb59ca8a0df3da14c7056
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-17 14:54:20 +0530

    Total Questions 126 updated code

--- C++/1207. Unique Number of Occurrences.cpp
+++ C++/1207. Unique Number of Occurrences.cpp
@@ -1,22 +1,22 @@
-class Solution {
-public:
-    bool uniqueOccurrences(vector<int>& arr) {
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
 
-        map<int, int> mp;
+//         map<int, int> mp;
 
-        for(int i = 0; i < arr.size(); i++) {
-            mp[arr[i]]++;
-        }
+//         for(int i = 0; i < arr.size(); i++) {
+//             mp[arr[i]]++;
+//         }
 
-        for(auto i : mp) {
-            for(auto j : mp) {
+//         for(auto i : mp) {
+//             for(auto j : mp) {
 
-                if(i.first != j.first && i.second == j.second) {
-                    return false;
-                }
-            }
-        }
+//                 if(i.first != j.first && i.second == j.second) {
+//                     return false;
+//                 }
+//             }
+//         }
 
-        return true;
-    }
-};
+//         return true;
+//     }
+// };

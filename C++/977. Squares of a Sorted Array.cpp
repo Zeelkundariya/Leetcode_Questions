d897d260c6bbed924898385f657eeb8b077176b0
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-17 07:33:45 +0530

    Total Questions-128 added time and space

--- C++/977. Squares of a Sorted Array.cpp
+++ C++/977. Squares of a Sorted Array.cpp
@@ -1,27 +1,29 @@
-class Solution {
-public:
-    vector<int> sortedSquares(vector<int>& nums) {
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
 
-        int left = 0;
-        int right = nums.size() - 1;
+//         int left = 0;
+//         int right = nums.size() - 1;
 
-        vector<int> ans(nums.size());
+//         vector<int> ans(nums.size());
 
-        int index = nums.size()-1;
+//         int index = nums.size()-1;
 
-        while(left <= right) {
+//         while(left <= right) {
 
-            if(nums[left] * nums[left] > nums[right]*nums[right]) {
-                ans[index] = nums[left]*nums[left];
-                left++;
-            }
-            else {
-                ans[index] = nums[right]*nums[right];
-                right--;
-            }
+//             if(nums[left] * nums[left] > nums[right]*nums[right]) {
+//                 ans[index] = nums[left]*nums[left];
+//                 left++;
+//             }
+//             else {
+//                 ans[index] = nums[right]*nums[right];
+//                 right--;
+//             }
 
-            index--;
-        }
-        return ans;
-    }
-};
+//             index--;
+//         }
+//         return ans;
+//     }
+// };
+// time 0(n)
+//space 0(n)

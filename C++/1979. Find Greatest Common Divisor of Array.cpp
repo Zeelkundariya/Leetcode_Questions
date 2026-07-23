8deb0931c5a91039e43161e6cfc3153eb3ea4fe5
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-23 23:44:23 +0530

    Added 1979

--- C++/1979. Find Greatest Common Divisor of Array.cpp
+++ C++/1979. Find Greatest Common Divisor of Array.cpp
@@ -6,7 +6,7 @@
     //             b =a%b;
     //             a = temp;
     //         }
-    //         return a;
+    //         return 
     //     }
     //     int findGCD(vector<int>& nums){
     //         int min = *min_element(nums.begin(), nums.end());

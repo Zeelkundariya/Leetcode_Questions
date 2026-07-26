cd30616761c7aeb3f6a34e9b02ef2c0c046e2a6e
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-26 21:56:32 +0530

    Added 904 added time

--- C++/904. Fruit Into Baskets.cpp
+++ C++/904. Fruit Into Baskets.cpp
@@ -1,24 +1,26 @@
-class Solution {
-public:
-    int totalFruit(vector<int>& fruits) {
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
 
-        map<int, int> mp;
+//         map<int, int> mp;
 
-        int left = 0;
-        int ans = 0;
+//         int left = 0;
+//         int ans = 0;
 
-        for(int right = 0; right < fruits.size(); right++) {
-            mp[fruits[right]]++;
-            while(mp.size() > 2) {
-                mp[fruits[left]]--;
-                if(mp[fruits[left]] == 0) {
-                    mp.erase(fruits[left]);
-                }
+//         for(int right = 0; right < fruits.size(); right++) {
+//             mp[fruits[right]]++;
+//             while(mp.size() > 2) {
+//                 mp[fruits[left]]--;
+//                 if(mp[fruits[left]] == 0) {
+//                     mp.erase(fruits[left]);
+//                 }
 
-                left++;
-            }
-            ans = max(ans, right - left + 1);
-        }
-        return ans;
-    }
-};
+//                 left++;
+//             }
+//             ans = max(ans, right - left + 1);
+//         }
+//         return ans;
+//     }
+// };
+
+//time 0(n)

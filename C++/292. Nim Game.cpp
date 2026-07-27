ab8f4fce347e4a61363de42b6f8ecf3d6625ed73
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-27 15:29:14 +0530

    added 292 questions added time

--- C++/292. Nim Game.cpp
+++ C++/292. Nim Game.cpp
@@ -1,10 +1,12 @@
-class Solution {
-public:
-    bool canWinNim(int n) {
+// class Solution {
+// public:
+//     bool canWinNim(int n) {
 
-        if(n%4== 0) {
-            return false;
-        }
-        return true;
-    }
-};
+//         if(n%4== 0) {
+//             return false;
+//         }
+//         return true;
+//     }
+// };
+
+//time 0(1)

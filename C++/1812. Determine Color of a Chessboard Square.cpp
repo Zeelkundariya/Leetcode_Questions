c5619f0cea5515ca89e42f737c2f0ef3cbf49d74
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-27 16:10:44 +0530

    added 1812 questions added time

--- C++/1812. Determine Color of a Chessboard Square.cpp
+++ C++/1812. Determine Color of a Chessboard Square.cpp
@@ -1,12 +1,14 @@
-class Solution {
-public:
-    bool squareIsWhite(string coordinates) {
-        int letter = coordinates[0] - 'a' + 1;
-        int number = coordinates[1] - '0';
+// class Solution {
+// public:
+//     bool squareIsWhite(string coordinates) {
+//         int letter = coordinates[0] - 'a' + 1;
+//         int number = coordinates[1] - '0';
         
-        if((letter + number) % 2 == 1) {
-            return true;
-        }
-        return false;
-    }
-};
+//         if((letter + number) % 2 == 1) {
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+//time0(1)

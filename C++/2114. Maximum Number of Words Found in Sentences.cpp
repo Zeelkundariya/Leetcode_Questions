d068b8d6ad5fc689be8cc03fcbbbd3b0524727bb
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-28 14:57:29 +0530

    added 2114 added time

--- C++/2114. Maximum Number of Words Found in Sentences.cpp
+++ C++/2114. Maximum Number of Words Found in Sentences.cpp
@@ -1,23 +1,25 @@
-class Solution {
-public:
-    int mostWordsFound(vector<string>& sentences) {
+// class Solution {
+// public:
+//     int mostWordsFound(vector<string>& sentences) {
 
-        int ans = 0;
+//         int ans = 0;
 
-        for(int i = 0; i < sentences.size(); i++) {
+//         for(int i = 0; i < sentences.size(); i++) {
 
-            int count = 1;
+//             int count = 1;
 
-            for(int j = 0; j < sentences[i].size(); j++) {
+//             for(int j = 0; j < sentences[i].size(); j++) {
 
-                if(sentences[i][j] == ' ') {
-                    count++;
-                }
-            }
+//                 if(sentences[i][j] == ' ') {
+//                     count++;
+//                 }
+//             }
 
-            ans = max(ans, count);
-        }
+//             ans = max(ans, count);
+//         }
 
-        return ans;
-    }
-};
+//         return ans;
+//     }
+// };
+
+//time 0(n)

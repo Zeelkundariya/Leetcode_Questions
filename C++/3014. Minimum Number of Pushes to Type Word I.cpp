a0bed8d922a54f2926905c7447171f3781d14935
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-30 14:53:53 +0530

    Added 3014 added time

--- C++/3014. Minimum Number of Pushes to Type Word I.cpp
+++ C++/3014. Minimum Number of Pushes to Type Word I.cpp
@@ -1,11 +1,13 @@
-class Solution {
-public:
-    int minimumPushes(string word) {
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
 
-        int ans=0;
-        for(int i=0;i<word.size();i++) {
-            ans +=(i/8)+1;
-        }
-        return ans;
-    }
-};
+//         int ans=0;
+//         for(int i=0;i<word.size();i++) {
+//             ans +=(i/8)+1;
+//         }
+//         return ans;
+//     }
+// };
+
+//time 0(n)

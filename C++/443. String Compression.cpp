36e666bf23a739f2029b5e8826e6345e242874f3
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-30 15:55:32 +0530

    Added 443 code

---
+++ C++/443. String Compression.cpp
@@ -0,0 +1,28 @@
+// class Solution {
+// public:
+//     int compress(vector<char>& chars) {
+
+//         int index =0;
+//         int i =0;
+
+//         while(i <chars.size()) {
+//             char current =chars[i];
+//             int count =0;
+
+//             while(i<chars.size() &&chars[i] ==current) {
+//                 count++;
+//                 i++;
+//             }
+//             chars[index] =current;
+//             index++;
+//             if(count > 1){
+//                 string s = to_string(count);
+//                 for(char ch : s){
+//                     chars[index] =ch;
+//                     index++;
+//                 }
+//             }
+//         }
+//         return index;
+//     }
+// };

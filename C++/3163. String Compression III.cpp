0782b7d7a0f37ebecd3231a0761feba65ef48773
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-30 15:36:14 +0530

    Added 3014 added time

--- C++/3163. String Compression III.cpp
+++ C++/3163. String Compression III.cpp
@@ -1,21 +1,23 @@
-class Solution {
-public:
-    string compressedString(string word) {
+// class Solution {
+// public:
+//     string compressedString(string word) {
 
-        string ans ="";
-        int i=0;
+//         string ans ="";
+//         int i=0;
 
-        while(i <word.size()) {
-            char ch =word[i];
-            int count =0;
+//         while(i <word.size()) {
+//             char ch =word[i];
+//             int count =0;
 
-            while(i<word.size() &&word[i] ==ch && count<9) {
-                count++;
-                i++;
-            }
-            ans +=to_string(count);
-            ans +=ch;
-        }
-        return ans;
-    }
-};
+//             while(i<word.size() &&word[i] ==ch && count<9) {
+//                 count++;
+//                 i++;
+//             }
+//             ans +=to_string(count);
+//             ans +=ch;
+//         }
+//         return ans;
+//     }
+// };
+
+//time 0(n)

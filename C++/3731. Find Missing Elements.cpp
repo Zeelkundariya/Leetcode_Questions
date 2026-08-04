f63709dcf6a5098529f0ef969dd7559c9a5a4b96
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-04 20:07:05 +0530

    Added 3731 added space

--- C++/3731. Find Missing Elements.cpp
+++ C++/3731. Find Missing Elements.cpp
@@ -1,17 +1,19 @@
 
-class Solution {
-public:
-    vector<int> findMissing(vector<int>& nums){
-        sort(nums.begin(),nums.end());
-        vector<int> ans;
-        for(int i=0; i<nums.size()-1;i++){
-            int current =nums[i] + 1;
-            while(current <nums[i + 1]){
-                ans.push_back(current);
-                current++;
-            }
-        }
+// class Solution {
+// public:
+//     vector<int> findMissing(vector<int>& nums){
+//         sort(nums.begin(),nums.end());
+//         vector<int> ans;
+//         for(int i=0; i<nums.size()-1;i++){
+//             int current =nums[i] + 1;
+//             while(current <nums[i + 1]){
+//                 ans.push_back(current);
+//                 current++;
+//             }
+//         }
 
-        return ans;
-    }
-};
+//         return ans;
+//     }
+// };
+
+//time 0(o log n)

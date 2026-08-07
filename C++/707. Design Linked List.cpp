9a1ffaddbd1f1816d8e40812ee5df946647b312e
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-07 05:40:29 +0530

    added 707 added time

--- C++/707. Design Linked List.cpp
+++ C++/707. Design Linked List.cpp
@@ -1,103 +1,105 @@
-class MyLinkedList {
-public:
+// class MyLinkedList {
+// public:
 
-    struct Node {
-        int val;
-        Node* next;
-        Node(int x) {
-            val = x;
-            next = NULL;
-        }
-    };
-    Node* head;
-    int size;
+//     struct Node {
+//         int val;
+//         Node* next;
+//         Node(int x) {
+//             val = x;
+//             next = NULL;
+//         }
+//     };
+//     Node* head;
+//     int size;
 
-    MyLinkedList(){
-        head = NULL;
-        size = 0;
-    }
+//     MyLinkedList(){
+//         head = NULL;
+//         size = 0;
+//     }
 
-    int get(int index){
-        if(index < 0 || index >= size)
-            return -1;
+//     int get(int index){
+//         if(index < 0 || index >= size)
+//             return -1;
 
-        Node* temp = head;
-        for(int i = 0; i < index; i++){
-            temp = temp->next;
-        }
+//         Node* temp = head;
+//         for(int i = 0; i < index; i++){
+//             temp = temp->next;
+//         }
 
-        return temp->val;
-    }
+//         return temp->val;
+//     }
 
-    void addAtHead(int val){
-        Node* newNode = new Node(val);
-        newNode->next = head;
-        head = newNode;
-        size++;
-    }
+//     void addAtHead(int val){
+//         Node* newNode = new Node(val);
+//         newNode->next = head;
+//         head = newNode;
+//         size++;
+//     }
 
-    void addAtTail(int val){
-        Node* newNode = new Node(val);
-        if(head == NULL){
-            head = newNode;
-            size++;
-            return;
-        }
-        Node* temp = head;
-        while(temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-        size++;
-    }
+//     void addAtTail(int val){
+//         Node* newNode = new Node(val);
+//         if(head == NULL){
+//             head = newNode;
+//             size++;
+//             return;
+//         }
+//         Node* temp = head;
+//         while(temp->next != NULL) {
+//             temp = temp->next;
+//         }
+//         temp->next = newNode;
+//         size++;
+//     }
 
-    void addAtIndex(int index, int val){
-        if(index > size)
-            return;
+//     void addAtIndex(int index, int val){
+//         if(index > size)
+//             return;
 
-        if(index <= 0){
-            addAtHead(val);
-            return;
-        }
-        if(index == size){
-            addAtTail(val);
-            return;
-        }
-        Node* temp = head;
+//         if(index <= 0){
+//             addAtHead(val);
+//             return;
+//         }
+//         if(index == size){
+//             addAtTail(val);
+//             return;
+//         }
+//         Node* temp = head;
 
-        for(int i =0; i<index-1;i++){
-            temp = temp->next;
-        }
+//         for(int i =0; i<index-1;i++){
+//             temp = temp->next;
+//         }
 
-        Node* newNode = new Node(val);
-        newNode->next = temp->next;
-        temp->next = newNode;
-        size++;
-    }
+//         Node* newNode = new Node(val);
+//         newNode->next = temp->next;
+//         temp->next = newNode;
+//         size++;
+//     }
 
-    void deleteAtIndex(int index){
-        if(index < 0 || index >= size)
-            return;
+//     void deleteAtIndex(int index){
+//         if(index < 0 || index >= size)
+//             return;
 
-        if(index == 0){
+//         if(index == 0){
 
-            Node* temp= head;
-            head = head->next;
-            delete temp;
-            size--;
+//             Node* temp= head;
+//             head = head->next;
+//             delete temp;
+//             size--;
 
-            return;
-        }
+//             return;
+//         }
 
-        Node* temp = head;
-        for(int i = 0;i<index-1;i++){
-            temp = temp->next;
-        }
+//         Node* temp = head;
+//         for(int i = 0;i<index-1;i++){
+//             temp = temp->next;
+//         }
 
-        Node* deleteNode = temp->next;
-        temp->next = deleteNode->next;
-        delete deleteNode;
+//         Node* deleteNode = temp->next;
+//         temp->next = deleteNode->next;
+//         delete deleteNode;
 
-        size--;
-    }
-};
+//         size--;
+//     }
+// };
+
+//time 0(n)

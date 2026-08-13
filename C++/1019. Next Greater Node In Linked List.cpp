6db6efb6dfea8ce26e7ca7b297711260cc2eea7d
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-13 15:17:37 +0530

    added 1019

---
+++ C++/1019. Next Greater Node In Linked List.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+
+        vector<int> ans;
+        stack<pair<int, int>> st;
+
+        int index = 0;
+        while(head != NULL) {
+            ans.push_back(0);
+
+            while(!st.empty() && head->val > st.top().first) {
+                ans[st.top().second] = head->val;
+                st.pop();
+            }
+            st.push({head->val, index});
+            index++;
+            head = head->next;
+        }
+        return ans;
+    }
+};

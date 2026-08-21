f2142dcf6eaeba6f068501ec1b64d5c23a752cfb
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-21 17:49:15 +0530

    added 36

--- C++/36. Valid Sudoku.cpp
+++ C++/36. Valid Sudoku.cpp
@@ -1,33 +1,33 @@
-class Solution {
-public:
-    bool isValidSudoku(vector<vector<char>>& board) {
+// class Solution {
+// public:
+//     bool isValidSudoku(vector<vector<char>>& board) {
 
-        bool row[9][10] = {};
-        bool col[9][10] = {};
-        bool box[9][10] = {};
+//         bool row[9][10] = {};
+//         bool col[9][10] = {};
+//         bool box[9][10] = {};
 
-        for(int i = 0; i < 9; i++) {
+//         for(int i = 0; i < 9; i++) {
 
-            for(int j = 0; j < 9; j++) {
+//             for(int j = 0; j < 9; j++) {
 
-                if(board[i][j] == '.')
-                    continue;
+//                 if(board[i][j] == '.')
+//                     continue;
 
-                int num = board[i][j] - '0';
+//                 int num = board[i][j] - '0';
 
-                // Find which 3x3 box
-                int boxIndex = (i / 3) * 3 + (j / 3);
+//                 // Find which 3x3 box
+//                 int boxIndex = (i / 3) * 3 + (j / 3);
 
-                // Check duplicate
-                if(row[i][num] || col[j][num] || box[boxIndex][num])
-                    return false;
+//                 // Check duplicate
+//                 if(row[i][num] || col[j][num] || box[boxIndex][num])
+//                     return false;
 
-                row[i][num] = true;
-                col[j][num] = true;
-                box[boxIndex][num] = true;
-            }
-        }
+//                 row[i][num] = true;
+//                 col[j][num] = true;
+//                 box[boxIndex][num] = true;
+//             }
+//         }
 
-        return true;
-    }
-};
+//         return true;
+//     }
+// };

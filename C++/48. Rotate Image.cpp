91eafd96f30466e086bd00feaa132a18402b69c9
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-25 15:20:32 +0530

    added 48 added time

--- C++/48. Rotate Image.cpp
+++ C++/48. Rotate Image.cpp
@@ -1,19 +1,22 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
         
-        int n = matrix.size();
+//         int n = matrix.size();
 
-        // Step 1: Transpose
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
+//         // Step 1: Transpose
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 swap(matrix[i][j], matrix[j][i]);
+//             }
+//         }
 
-        // Step 2: Reverse each row
-        for (int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-    }
-};
+//         // Step 2: Reverse each row
+//         for (int i = 0; i < n; i++) {
+//             reverse(matrix[i].begin(), matrix[i].end());
+//         }
+//     }
+// };
+
+
+//time 0(n2)

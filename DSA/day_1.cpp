87d48ed790a025e8f1e59d681e356fddd343b5a3
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-09-01 11:53:01 +0530

    added day-1.cpp code

--- DSA/day_1.cpp
+++ DSA/day_1.cpp
@@ -1,10 +1,29 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int x = 10;
+//     cout << &x << "\n";
+//     int *y = &x;
+//     cout << y;
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
+struct Box{
+    int value;
+    Box* address;
+};
+
 int main(){
-    int x = 10;
-    cout << &x << "\n";
-    int *y = &x;
-    cout << y;
+
+    Box* b1 = new Box();
+    b1->value = 10;
+    Box* b2 = new Box();
+    b1->address = b2;
+    cout << b1->address << endl;
     return 0;
 }
